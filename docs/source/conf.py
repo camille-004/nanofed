0b475a62af884432fd65babdca99547222e16438
camille-004 <dunningcamille@gmail.com>
2024-12-05 02:50:30 -0800

    chore: bump version to 0.1.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "nanofed"
 copyright = "2024, Camille Dunning"
 author = "Camille Dunning"
-release = "0.1.1"
+release = "0.1.2"
 
 # -- General configuration
 extensions = [
